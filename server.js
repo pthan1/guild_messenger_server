46653fcdae69349c7f1aa0ad8a64229ca5540b96
Phil Than <82873669+pthan1@users.noreply.github.com>
2022-01-19 21:08:49 -0700

    Seed user and conversation data

--- server.js
+++ server.js
@@ -2,10 +2,46 @@ const express = require('express');
 const app = express();
 
 app.set('port', process.env.PORT || 3001);
-app.locals.title = 'Guild Messenger App ';
+app.locals.title = 'Guild Messenger App Server';
+app.locals.users = [
+  {
+    user_id: 1,
+    user_name: 'ProfessorX',
+    conversation_ids: [ 1234 ],
+  },
+  {
+    user_id: 2,
+    user_name: 'Angel',
+    conversation_ids: [ 1234 ]
+  }
+]
+
+app.locals.conversations = [
+      {
+        conversation_id: 1234,
+        user_ids: [ 1, 2],
+        message_log: [
+          {
+            sender: 1,
+            message: 'Hello, this is Professor Charles Xavier.',
+            time_sent: '2022-01-20T04:06:24.448Z'
+          },
+          {
+            sender: 2,
+            message: 'Hello, this is Angel.',
+            time_sent: '2022-01-20T04:07:26.768Z'
+          },
+           {
+            sender: 1,
+            message: 'Where is your homework, Angel?',
+            time_sent: '2022-01-20T04:08:22.768Z'
+          }
+        ]
+      }
+];
+
 
 app.get('/', (request, response) => {
-  response.send('Oh hey Pet Box');
 });
 
 app.listen(app.get('port'), () => {
